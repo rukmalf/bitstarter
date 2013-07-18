120484f6c62addea0126e0aa329866d9e47e0757
Ubuntu <ubuntu@ip-172-31-18-141.us-west-2.compute.internal>
2013-07-18 07:37:56 +0000

    Add index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 // read the file
-var inFile = "index.html";
+var inFile = "./index.html";
 var buffer = fs.readFileSync(inFile);
 
 app.get('/', function(request, response) {
